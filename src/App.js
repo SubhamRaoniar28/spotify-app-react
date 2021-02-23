ec46e38cc164692445c95baa7dc27315ee1ecb76
SubhamRaoniar28 <41485604+SubhamRaoniar28@users.noreply.github.com>
2021-02-24 05:05:20 +0530

    added function getTokenFromUrl, useEffect to set token in state and if token present, view Player Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Login from './components/Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+
+  const [token, setToken] = useState(null);
+
+  //after being redirected from login page to localhost:3000, app.js is reloading
+  //and is firing of this code to get the access token 
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+  }, [])
+
+
   return (
     <div className="app">
-      <Login />
+      {
+        token ? (
+          //<Player />
+          <h1>I am logged in!</h1>
+        ) : (
+          <Login />
+        )
+      }
+
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './Login.css'
 import Logo from '../images/spotify-logo.jpg';
+import { loginUrl } from '../spotify';
 
 function Login() {
     return (
         <div className="login">
 
             <img src={Logo} alt="spotify logo" />
-            <a>LOGIN WITH SPOTIFY</a>  
+            <a href={loginUrl}>LOGIN WITH SPOTIFY</a>  
 
         </div>
     )
